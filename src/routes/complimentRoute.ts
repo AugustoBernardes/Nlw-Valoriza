fadaed4570255e0bb7ebee3376f7bc8f26c563c0
Augusto <augustobdev@outlook.com>
2022-03-09 09:28:18 -0300

    feature - List compliments route done

--- src/routes/complimentRoute.ts
+++ src/routes/complimentRoute.ts
@@ -1,3 +1,4 @@
+import { ListUserReceiverComplimentsController,ListUserSenderComplimentsController } from "../controllers/ListComplimentsController"
 import { CreateComplimentController } from '../controllers/CreateComplimentController';
 import { ensureAuthenticated } from "../middlewares/ensureAuthenticated"
 import { Router } from "express"
@@ -6,6 +7,12 @@ const complimentsRouter = Router();
 
 const createComplimentController = new CreateComplimentController();
 
+const listUserSenderComplimentsController = new ListUserSenderComplimentsController();
+const listUserReceiverComplimentsController = new ListUserReceiverComplimentsController();
+
 complimentsRouter.post("/newcompliment",ensureAuthenticated,createComplimentController.handle)
+
+complimentsRouter.get("/sent",ensureAuthenticated, listUserSenderComplimentsController.handle )
+complimentsRouter.get("/received",ensureAuthenticated, listUserReceiverComplimentsController.handle )
 
 export { complimentsRouter }
